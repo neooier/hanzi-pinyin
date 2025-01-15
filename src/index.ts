c3d135c5400fc6bf758e4d08f3935661305089c0
Jorylee <1747358809@qq.com>
2025-01-15 19:25:43 +0800

    fix(src): 优化 pinyin 函数返回结果
    - 在 pinyin 函数中，当 dict[char] 为 undefined 时直接返回 undefined
    - 使用 Set 结构去除重复的拼音结果，避免出现相同拼音

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import _dict from './dict.json'
 
 const dict = _dict as { [key: string]: string[] }
 export function pinyin(char: string): string[] | undefined {
-    return dict[char]?.map(p => unidecode(p))
+    const tmp = dict[char]?.map(p => unidecode(p))
+    return tmp === undefined ? undefined : Array.from(new Set(tmp))
 }
 
 export function detailedPinyin(char: string): string[] | undefined {
